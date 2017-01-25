d9ae046f1ee4d479fc58aedc0eb2c73e3dd87809
Emanuel Amiguinho <emanuelamiguinho@outlook.com>
2017-01-25 17:16:51 +0000

    seems like .gitignore it's not working but fuck it

--- SwarmAthon/PlatformServices/CustomNavigationService.cs
+++ SwarmAthon/PlatformServices/CustomNavigationService.cs
@@ -1,11 +1,12 @@
 using Android.App;
+using Android.Support.V4.App;
 using SwarmAthon.Core.Interfaces.Services.PlatformSpecific;
 
 namespace SwarmAthon.PlatformServices
 {
     public class CustomNavigationService : ICustomNavigationService
     {
-        private Activity _activity;
+        private ActivityCompat _activity;
 
         public void SetCurrentActivity(ActivityCompat activity)
         {
